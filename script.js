02b7d76484993a014f5e7752aee79ad1f9059ad5
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-06-07 14:13:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+let time = document.getElementById("timer");
+
+setInterval(() => {
+	let date=new Date();
+    let month= date.getMonth()+1;
+    let day= date.getDate();
+    let year= date.getFullYear();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+	time.innerText= `${month}/${day}/${year}, ${hours}:${minutes}:${seconds} PM`;
+},1000)
